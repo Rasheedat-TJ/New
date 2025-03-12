5a1b670a9aec718d3400a032f3207054fd633941
Rasheedat-TJ <137012075+Rasheedat-TJ@users.noreply.github.com>
2025-03-12 22:32:35 +0100

    Update python.py
    Week 1 Python Assignment

--- python.py
+++ python.py
@@ -1,1 +1,28 @@
-#calculator
+#Create a simple Python program that asks the user to input two numbers and a mathematical operation (addition, subtraction, multiplication, or division).Perform the operation based on the user's input and print the result.
+def calculator():
+    num1 = float(input("Enter the first Number: "))
+    num2 = float(input("Enter the second Number: "))
+    operation = input("Enter the operation (+, -, * or /): ")
+    if operation == "+":
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+
+    elif operation == "-":
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+            
+    elif operation == "*":
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+
+    elif operation == "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("Error; divison by zero is not allowed!")
+
+    else:
+        print("Error: Input the correct details!")
+
+calculator()
